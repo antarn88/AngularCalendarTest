8416a7ca1d9f46ff944139f7cff36ec29dc92cbe
Arnold <antaloczi.arnold@gmail.com>
2022-01-05 10:53:34 +0100

    Base app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './widget/navbar/navbar.component';
+import { HomeComponent } from './page/home/home.component';
+import { CalendarComponent } from './widget/calendar/calendar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
