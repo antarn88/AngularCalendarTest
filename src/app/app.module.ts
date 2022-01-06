6c00c7692b6d0498487be2e93ad59fcf2313e652
Arnold <antaloczi.arnold@gmail.com>
2022-01-06 12:49:59 +0100

    Set a simple angular calendar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { FlatpickrModule } from 'angularx-flatpickr';
+import { NgbModalModule, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import 'flatpickr/dist/flatpickr.css'; // you may need to adjust the css import depending on your build tool
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,10 +27,16 @@ import { CalendarComponent } from './widget/calendar/calendar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     CalendarModule.forRoot({
       provide: DateAdapter,
       useFactory: adapterFactory,
     }),
+    FormsModule,
+    FlatpickrModule.forRoot(),
+    NgbModule,
+    CommonModule,
+    NgbModalModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
